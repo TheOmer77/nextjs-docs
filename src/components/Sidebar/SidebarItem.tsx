84721b42d6e4e1379e8fa28f427187f359d065c4
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-04-27 16:21:26 +0300

    Remove unnecessary sidebar types, use Doc type instead

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -6,40 +6,44 @@ import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
 
-import type { NavItem } from './types';
+import { type Doc, allDocs } from 'contentlayer/generated';
 import styles from './index.module.scss';
 
-const SidebarItem = ({ item }: { item: NavItem }) => {
+const SidebarItem = ({ doc }: { doc: Doc }) => {
   const pathname = usePathname();
-  const isActive = useMemo(
-    () => 'href' in item && item.href === pathname,
-    [item, pathname]
+  const isActive = useMemo(() => doc.url === pathname, [doc, pathname]);
+  const children = useMemo(
+    () =>
+      allDocs.filter(childDoc =>
+        childDoc._raw.flattenedPath.startsWith(`${doc._raw.flattenedPath}/`)
+      ),
+    [doc._raw.flattenedPath]
   );
 
-  return 'items' in item ? (
+  return children.length > 0 ? (
     // Item is a category
     <li className={styles.category}>
       <details>
         <summary className={styles.button}>
-          {item.label}
+          {doc.title}
           <ChevronRightIcon />
         </summary>
         <ul className={styles.list}>
-          {item.items.map(item => (
-            <SidebarItem key={item.id} item={item} />
+          {children.map(childDoc => (
+            <SidebarItem key={childDoc._id} doc={childDoc} />
           ))}
         </ul>
       </details>
     </li>
   ) : (
     // Item is a link
-    <li key={item.href}>
+    <li key={doc._id}>
       <Link
-        href={item.href}
+        href={doc.url}
         className={clsx(styles.button, isActive && styles.active)}
         aria-current={isActive ? 'page' : undefined}
       >
-        {item.label}
+        {doc.title}
       </Link>
     </li>
   );
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,15 @@
+import { allDocs } from 'contentlayer/generated';
 import SidebarItem from './SidebarItem';
-import type { NavItem } from './types';
 import styles from './index.module.scss';
 
-const Sidebar = ({ items }: { items: NavItem[] }) => (
+const Sidebar = () => (
   <nav className={styles.container}>
     <ul className={styles.list}>
-      {items.map(item => (
-        <SidebarItem key={item.id} item={item} />
-      ))}
+      {allDocs
+        .filter(doc => doc._raw.flattenedPath.split('/').length < 2)
+        .map(doc => (
+          <SidebarItem key={doc._id} doc={doc} />
+        ))}
     </ul>
   </nav>
 );
