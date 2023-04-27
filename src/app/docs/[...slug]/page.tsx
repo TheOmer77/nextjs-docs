e5f4620610eea8f201a9402ff8fca206a5836a0a
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-04-27 15:54:50 +0300

    Replace dummy sidebar content with real content

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -3,7 +3,6 @@ import { allDocs } from 'contentlayer/generated';
 import { getMDXComponent } from 'next-contentlayer/hooks';
 
 import Sidebar from 'components/Sidebar';
-import dummySidebarItems from 'constants/dummySidebarItems';
 import layoutStyles from './styles.module.scss';
 import markupStyles from 'styles/markup.module.scss';
 
@@ -21,11 +20,36 @@ const DocLayout = ({ params }: { params: { slug: string[] } }) => {
   );
   if (!doc) return notFound();
 
+  const sidebarItems = allDocs
+    .filter(doc => doc._raw.flattenedPath.split('/').length < 2)
+    .map(doc => ({
+      id: doc._raw.flattenedPath,
+      label: doc.title,
+      href: doc.url,
+      ...(allDocs.some(childDoc =>
+        childDoc._raw.flattenedPath.startsWith(`${doc._raw.flattenedPath}/`)
+      )
+        ? {
+            items: allDocs
+              .filter(childDoc =>
+                childDoc._raw.flattenedPath.startsWith(
+                  `${doc._raw.flattenedPath}/`
+                )
+              )
+              .map(childDoc => ({
+                id: childDoc._raw.flattenedPath,
+                label: childDoc.title,
+                href: childDoc.url,
+              })),
+          }
+        : {}),
+    }));
+
   const MdxContent = getMDXComponent(doc.body.code);
 
   return (
     <div className={layoutStyles.container}>
-      <Sidebar items={dummySidebarItems} />
+      <Sidebar items={sidebarItems} />
       <article className={layoutStyles.article}>
         <header className={layoutStyles.header}>
           <h1 className={layoutStyles.title}>{doc.title}</h1>
