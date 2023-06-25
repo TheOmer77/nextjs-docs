ccb2e7cc6e81d329fde46c966ee49806d8a6dad6
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-06-25 12:14:04 +0300

    Remove useless links from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import Link from 'next/link';
-import { allDocs } from 'contentlayer/generated';
-
 const Home = () => {
   return (
-    <div>
-      <h1>App</h1>
-
-      <ul>
-        {allDocs.map(doc => (
-          <li key={doc._id}>
-            <Link href={doc.url}>{doc.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <header>
+      <h1>Hello world!</h1>
+    </header>
   );
 };
 
