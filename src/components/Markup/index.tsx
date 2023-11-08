41972400459b57f109320027af9cc0764dfda71b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-08 16:16:18 +0200

    Add Tailwind typography plugin + related config

--- src/components/Markup/index.tsx
+++ src/components/Markup/index.tsx
@@ -1,12 +1,10 @@
 import { type Doc } from 'contentlayer/generated';
 import { getMDXComponent } from 'next-contentlayer/hooks';
 
-import styles from './index.module.scss';
-
 const Markup = ({ doc }: { doc: Doc }) => {
   const MdxContent = getMDXComponent(doc.body.code);
   return (
-    <div className={styles.container}>
+    <div className='prose dark:prose-invert mx-auto max-w-3xl py-8'>
       <MdxContent />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 const shades = [50, ...[...Array(9).keys()].map(key => (key + 1) * 100), 950];
@@ -10,6 +11,44 @@ const config: Config = {
         sans: ['var(--font-family)', 'sans-serif'],
         mono: ['var(--font-family-mono)', 'monospace'],
       },
+      typography: () => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': 'rgb(var(--color-neutral-800))',
+            '--tw-prose-headings': 'rgb(var(--color-neutral-900))',
+            '--tw-prose-lead': 'rgb(var(--color-neutral-700))',
+            '--tw-prose-links': 'rgb(var(--color-neutral-900))',
+            '--tw-prose-bold': 'rgb(var(--color-neutral-900))',
+            '--tw-prose-counters': 'rgb(var(--color-neutral-600))',
+            '--tw-prose-bullets': 'rgb(var(--color-neutral-400))',
+            '--tw-prose-hr': 'rgb(var(--color-neutral-300))',
+            '--tw-prose-quotes': 'rgb(var(--color-neutral-900))',
+            '--tw-prose-quote-borders': 'rgb(var(--color-neutral-300))',
+            '--tw-prose-captions': 'rgb(var(--color-neutral-700))',
+            '--tw-prose-code': 'rgb(var(--color-neutral-900))',
+            '--tw-prose-pre-code': 'rgb(var(--color-neutral-100))',
+            '--tw-prose-pre-bg': 'rgb(var(--color-neutral-900))',
+            '--tw-prose-th-borders': 'rgb(var(--color-neutral-300))',
+            '--tw-prose-td-borders': 'rgb(var(--color-neutral-200))',
+            '--tw-prose-invert-body': 'rgb(var(--color-neutral-200))',
+            '--tw-prose-invert-headings': 'rgb(var(--color-neutral-50))',
+            '--tw-prose-invert-lead': 'rgb(var(--color-neutral-300))',
+            '--tw-prose-invert-links': 'rgb(var(--color-neutral-50))',
+            '--tw-prose-invert-bold': 'rgb(var(--color-neutral-50))',
+            '--tw-prose-invert-counters': 'rgb(var(--color-neutral-400))',
+            '--tw-prose-invert-bullets': 'rgb(var(--color-neutral-600))',
+            '--tw-prose-invert-hr': 'rgb(var(--color-neutral-700))',
+            '--tw-prose-invert-quotes': 'rgb(var(--color-neutral-100))',
+            '--tw-prose-invert-quote-borders': 'rgb(var(--color-neutral-700))',
+            '--tw-prose-invert-captions': 'rgb(var(--color-neutral-400))',
+            '--tw-prose-invert-code': 'rgb(var(--color-neutral-50))',
+            '--tw-prose-invert-pre-code': 'rgb(var(--color-neutral-300))',
+            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
+            '--tw-prose-invert-th-borders': 'rgb(var(--color-neutral-600))',
+            '--tw-prose-invert-td-borders': 'rgb(var(--color-neutral-700))',
+          },
+        },
+      }),
     },
     colors: {
       white: '#fff',
@@ -23,14 +62,14 @@ const config: Config = {
               ...obj,
               [shade]: `rgb(var(--color-${colorName}-${shade}) / <alpha-value>)`,
             }),
-            {},
+            {}
           ),
         }),
-        {},
+        {}
       ),
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
 
 export default config;
