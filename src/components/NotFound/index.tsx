49d3b11e3b8195c075ce6b74173eae7e9ec77ed1
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-09 17:52:21 +0200

    Tweak text color in NotFound

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -3,7 +3,7 @@ const NotFound = () => (
     <span className='text-[16rem] font-extrabold leading-none text-danger-800 dark:text-danger-200'>
       404
     </span>
-    <span className='text-center text-xl text-neutral-600 dark:text-neutral-400'>
+    <span className='text-center text-xl text-neutral-700 dark:text-neutral-300'>
       I have no idea what it is that you were looking for - but it&apos;s
       definitely not here.
     </span>
