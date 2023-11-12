b60a907cd49e67f31c2d4b5b2928689c59f822c8
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-12 23:18:13 +0200

    Revamp home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-const Home = () => {
-  return (
-    <header>
-      <h1>Hello world!</h1>
-    </header>
-  );
-};
+import Link from 'next/link';
+
+const Home = () => (
+  <div className='mx-auto flex min-h-[calc(100dvh-4rem)] w-full max-w-3xl flex-col items-start justify-center gap-4'>
+    <h1 className='text-7xl font-extrabold tracking-tight'>Hello world!</h1>
+    <p className='text-lg text-neutral-700 dark:text-neutral-300'>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+      nesciunt soluta? Repellat reiciendis facere adipisci, amet eligendi dolore
+      quasi voluptas.
+    </p>
+    <Link
+      href='/docs/sample'
+      className='flex h-12 items-center justify-center rounded-lg
+bg-primary-main px-6 text-lg font-medium text-primary-contrast'
+    >
+      Get started
+    </Link>
+  </div>
+);
 
 export default Home;
