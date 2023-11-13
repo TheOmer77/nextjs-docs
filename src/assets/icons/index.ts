b9a7cf2d56919721068b790974e2ce62a4877258
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-13 14:17:21 +0200

    Add menu icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as ChevronRightIcon } from 'assets/icons/chevron_right.svg?react';
+export { default as MenuIcon } from 'assets/icons/menu.svg?react';
 export { default as SearchIcon } from 'assets/icons/search.svg?react';
