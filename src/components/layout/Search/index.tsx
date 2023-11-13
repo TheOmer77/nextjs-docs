14595d17ae255f79e84688abc68dc40cdb1b7db4
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-13 13:42:17 +0200

    Make search an icon button on mobile

--- src/components/layout/Search/index.tsx
+++ src/components/layout/Search/index.tsx
@@ -4,14 +4,16 @@ const Search = () => {
   return (
     <div>
       <button
-        className='flex h-10 w-64 flex-row items-center gap-2 rounded-lg
-bg-neutral-50 px-4 text-neutral-700 state-layer
+        className='flex h-10 w-10 select-none flex-row items-center
+justify-center gap-2 rounded-lg text-neutral-700 state-layer
 hover:state-layer-neutral-500/20 focus-visible:outline-none
 focus-visible:state-layer-neutral-500/20 active:state-layer-neutral-500/30
-dark:bg-neutral-900 dark:text-neutral-300'
+dark:text-neutral-300 sm:w-64 sm:justify-start sm:bg-neutral-50 sm:px-4
+dark:sm:bg-neutral-900 [&>svg]:text-lg [&>svg]:sm:text-base'
+        aria-label='Search docs'
       >
         <SearchIcon />
-        Search docs...
+        <span className='hidden sm:inline'>Search docs...</span>
       </button>
     </div>
   );
