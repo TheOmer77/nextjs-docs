d6b6ae3c229e5dab8ee837d8d4f43948a86657da
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-13 14:19:15 +0200

    Add nav menu icon on mobile

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -1,13 +1,18 @@
 import { NavHeader } from './NavHeader';
 import Search from '../Search';
+import IconButton from 'components/general/IconButton';
+import { MenuIcon } from 'assets/icons';
 
 export const Nav = () => {
   return (
     <nav
       className='fixed start-1/2 top-0 z-10 mx-auto flex h-16 w-full max-w-8xl
--translate-x-1/2 flex-row justify-between bg-white dark:bg-neutral-950
-md:bg-transparent dark:md:bg-transparent'
+-translate-x-1/2 flex-row items-center justify-between bg-white
+dark:bg-neutral-950 md:bg-transparent dark:md:bg-transparent'
     >
+      <IconButton aria-label='Open sidebar'>
+        <MenuIcon />
+      </IconButton>
       <NavHeader />
       <div
         className='flex grow flex-row items-center justify-end bg-white px-4
