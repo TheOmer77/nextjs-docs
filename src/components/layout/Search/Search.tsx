1a00807fa1d790a7c88c8ab6c9f0ffc47202d018
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-22 21:21:56 +0200

    Add Search dialog

--- src/components/layout/Search/Search.tsx
+++ src/components/layout/Search/Search.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+
 import { SearchButton } from './SearchButton';
+import SearchDialog from './SearchDialog';
 
 export const Search = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <SearchButton />
+      <SearchButton onClick={() => setOpen(true)} />
+      <SearchDialog open={open} onOpenChange={setOpen} />
     </>
   );
 };
---
+++ src/components/layout/Search/SearchDialog.tsx
@@ -0,0 +1,36 @@
+import {
+  Dialog as DialogRoot,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  type DialogProps,
+} from '@radix-ui/react-dialog';
+
+import { cn } from 'utils';
+
+const SearchDialog = ({ open, onOpenChange, children }: DialogProps) => {
+  return (
+    <DialogRoot open={open} onOpenChange={onOpenChange}>
+      <DialogPortal>
+        <DialogOverlay
+          className='fixed inset-0 z-50 bg-black/50
+data-[state=closed]:animate-fade-out data-[state=open]:animate-fade-in'
+        />
+        <DialogContent
+          className='data-[state=closed]:animate-zoom-out data-[state=open]:animate-zoom-in fixed start-1/2 top-1/2 z-50 grid
+w-[calc(100%-2rem)] max-w-md -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg
+bg-white shadow-lg shadow-neutral-950/20
+[--zoom-translate-x:-50%] [--zoom-translate-y:-50%]
+rtl:translate-x-1/2 rtl:[--zoom-translate-x:50%] dark:bg-neutral-900'
+        >
+          <div className='p-4 text-center text-neutral-500'>
+            TODO: Search here
+          </div>
+          {children}
+        </DialogContent>
+      </DialogPortal>
+    </DialogRoot>
+  );
+};
+
+export default SearchDialog;
