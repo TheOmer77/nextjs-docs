50499aa8679b32daa30edcfc7c895feb5f556265
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-22 19:56:15 +0200

    Fix RTL bug in Nav

--- src/components/layout/Nav/Nav.tsx
+++ src/components/layout/Nav/Nav.tsx
@@ -11,7 +11,8 @@ export const Nav = ({ onMenuClick }: NavProps) => (
   <nav
     className='fixed start-1/2 top-0 z-10 mx-auto flex h-16 w-full max-w-8xl
 -translate-x-1/2 flex-row items-center justify-between bg-white
-dark:bg-neutral-950 md:bg-transparent dark:md:bg-transparent'
+rtl:translate-x-1/2 dark:bg-neutral-950 md:bg-transparent
+dark:md:bg-transparent'
   >
     <IconButton
       aria-label='Open sidebar'
