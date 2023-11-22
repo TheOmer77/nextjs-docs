3896adcd24a8bab7f2286878273a9794211518e5
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-23 00:17:07 +0200

    Add ctrl+k & cmd+k shortcut to open search

--- src/components/layout/Search/Search.tsx
+++ src/components/layout/Search/Search.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { SearchButton } from './SearchButton';
 import SearchDialog from './SearchDialog';
 
 export const Search = () => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if ((event.ctrlKey || event.metaKey) && event.key === 'k') {
+        event.preventDefault();
+        setOpen(true);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return (
     <>
