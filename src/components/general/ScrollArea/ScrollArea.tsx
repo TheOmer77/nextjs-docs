0effacfd7f1b11ac714c30cdfdfa79682253fb2b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-22 19:56:04 +0200

    Revamp ScrollArea & add RTL support

---
+++ src/components/general/ScrollArea/ScrollArea.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from 'react';
+import {
+  ScrollArea as ScrollAreaRoot,
+  ScrollAreaViewport,
+  ScrollAreaCorner,
+} from '@radix-ui/react-scroll-area';
+
+import { cn } from 'utils';
+import { ScrollBar } from './ScrollBar';
+
+export const ScrollArea = forwardRef<
+  ElementRef<typeof ScrollAreaRoot>,
+  ComponentPropsWithoutRef<typeof ScrollAreaRoot>
+>(({ className, children, ...props }, ref) => (
+  <ScrollAreaRoot
+    {...props}
+    ref={ref}
+    className={cn('relative overflow-hidden [direction:inherit]', className)}
+  >
+    <ScrollAreaViewport className='h-full w-full rounded-[inherit]'>
+      {children}
+    </ScrollAreaViewport>
+    <ScrollBar />
+    <ScrollAreaCorner />
+  </ScrollAreaRoot>
+));
+ScrollArea.displayName = ScrollAreaRoot.displayName;
