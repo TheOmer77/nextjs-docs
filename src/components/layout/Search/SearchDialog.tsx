85dec360ba656219b48f888cf2a7fe471c45a0ad
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-23 00:20:20 +0200

    Remove outline when opening search dialog

--- src/components/layout/Search/SearchDialog.tsx
+++ src/components/layout/Search/SearchDialog.tsx
@@ -17,11 +17,12 @@ const SearchDialog = ({ open, onOpenChange, children }: DialogProps) => {
 data-[state=closed]:animate-fade-out data-[state=open]:animate-fade-in'
         />
         <DialogContent
-          className='data-[state=closed]:animate-zoom-out data-[state=open]:animate-zoom-in fixed start-1/2 top-1/2 z-50 grid
-w-[calc(100%-2rem)] max-w-md -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg
-bg-white shadow-lg shadow-neutral-950/20
-[--zoom-translate-x:-50%] [--zoom-translate-y:-50%]
-rtl:translate-x-1/2 rtl:[--zoom-translate-x:50%] dark:bg-neutral-900'
+          className='fixed start-1/2 top-1/2 z-50 grid w-[calc(100%-2rem)]
+max-w-md -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg bg-white shadow-lg
+shadow-neutral-950/20 [--zoom-translate-x:-50%] [--zoom-translate-y:-50%]
+focus-visible:outline-none data-[state=closed]:animate-zoom-out
+data-[state=open]:animate-zoom-in rtl:translate-x-1/2
+rtl:[--zoom-translate-x:50%] dark:bg-neutral-900'
         >
           <div className='p-4 text-center text-neutral-500'>
             TODO: Search here
