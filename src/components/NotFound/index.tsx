b1bb239531b9a0891b018c0c44ba91f15612969e
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-24 14:14:03 +0200

    Tweak 404 number tracking & formatting

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,6 +1,13 @@
 const NotFound = () => (
-  <div className='flex min-h-[calc(100dvh-4rem)] w-full flex-col items-center justify-center'>
-    <h1 className='text-9xl font-extrabold leading-none text-danger-800 dark:text-danger-200 sm:text-[16rem] md:text-[12rem] lg:text-[16rem]'>
+  <div
+    className='flex min-h-[calc(100dvh-4rem)] w-full flex-col items-center
+justify-center'
+  >
+    <h1
+      className='text-9xl font-extrabold leading-none tracking-tight
+text-danger-800 dark:text-danger-200 sm:text-[16rem] md:text-[12rem]
+lg:text-[16rem]'
+    >
       404
     </h1>
     <p className='text-center text-xl text-neutral-700 dark:text-neutral-300'>
