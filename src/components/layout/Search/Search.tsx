a1f4ab574717f0fe6d20d0c2eae5f2e774f66eee
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-29 23:27:45 +0200

    Add categories to Search

--- src/components/layout/Search/Search.tsx
+++ src/components/layout/Search/Search.tsx
@@ -1,19 +1,56 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-import { allDocs } from 'contentlayer/generated';
+import { allDocs, config, type Doc } from 'contentlayer/generated';
 
 import { SearchButton } from './SearchButton';
 import SearchDialog from './SearchDialog';
 import { SearchItem } from './SearchItem';
+import { SearchGroup } from './SearchGroup';
+
+const filteredDocs = allDocs
+  .filter(doc => {
+    if (!doc._raw.flattenedPath.includes('/')) return true;
+
+    const parentFolder = doc._raw.flattenedPath
+      .split('/')
+      .slice(0, -1)
+      .join('/');
+    return !allDocs.some(
+      d =>
+        d._raw.flattenedPath === parentFolder &&
+        d._id.startsWith(`${parentFolder}/index.`)
+    );
+  })
+  .sort((a, b) => (a._raw.flattenedPath > b._raw.flattenedPath ? 1 : -1));
+
+const uncategorizedDocs = filteredDocs.filter(
+    doc =>
+      typeof doc.category !== 'string' ||
+      !Object.keys(config.categories).includes(doc.category)
+  ),
+  docsByCategory = {
+    ...(uncategorizedDocs.length > 0 ? { _: uncategorizedDocs } : {}),
+    ...Object.keys(config.categories).reduce(
+      (obj, category) => ({
+        ...obj,
+        [category]: filteredDocs.filter(doc => doc.category === category),
+      }),
+      {} as { [key: string]: Doc[] }
+    ),
+  };
 
 export const Search = () => {
   const [open, setOpen] = useState(false);
   const router = useRouter();
 
-  const filteredDocs = allDocs
-    .filter(doc => doc._raw.flattenedPath.split('/').length < 3)
-    .sort((a, b) => (a._raw.flattenedPath > b._raw.flattenedPath ? 1 : -1));
+  const handleDocSelect = useCallback(
+    (doc: Doc) => {
+      setOpen(false);
+      router.push(doc.url);
+    },
+    [router]
+  );
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -33,15 +70,44 @@ export const Search = () => {
     <>
       <SearchButton onClick={() => setOpen(true)} />
       <SearchDialog open={open} onOpenChange={setOpen}>
-        {filteredDocs.map(doc => (
-          <SearchItem
-            key={doc._id}
-            doc={doc}
-            onSelect={() => {
-              setOpen(false);
-              router.push(doc.url);
-            }}
-          />
+        {Object.keys(docsByCategory).map(category => (
+          <SearchGroup
+            key={category}
+            {...(category !== '_'
+              ? { heading: config.categories[category] }
+              : {})}
+          >
+            {docsByCategory[category as keyof typeof docsByCategory]?.map(
+              doc => {
+                const children = allDocs
+                  .filter(childDoc =>
+                    childDoc._raw.flattenedPath.startsWith(
+                      `${doc._raw.flattenedPath}/`
+                    )
+                  )
+                  .sort((a, b) =>
+                    a._raw.flattenedPath > b._raw.flattenedPath ? 1 : -1
+                  );
+                return (
+                  <Fragment key={doc._id}>
+                    <SearchItem
+                      key={doc._id}
+                      doc={doc}
+                      onSelect={() => handleDocSelect(doc)}
+                    />
+                    {children.map(childDoc => (
+                      <SearchItem
+                        key={childDoc._id}
+                        doc={childDoc}
+                        parentDoc={doc}
+                        onSelect={() => handleDocSelect(childDoc)}
+                      />
+                    ))}
+                  </Fragment>
+                );
+              }
+            )}
+          </SearchGroup>
         ))}
       </SearchDialog>
     </>
--- src/components/layout/Search/SearchItem.tsx
+++ src/components/layout/Search/SearchItem.tsx
@@ -10,22 +10,31 @@ import type { Doc } from 'contentlayer/generated';
 export interface SearchItemProps
   extends ComponentPropsWithoutRef<typeof CommandItem> {
   doc: Doc;
+  parentDoc?: Doc;
 }
 
 export const SearchItem = forwardRef<
   ElementRef<typeof CommandItem>,
   SearchItemProps
->(({ doc, ...props }, ref) => (
+>(({ doc, parentDoc, ...props }, ref) => (
   <CommandItem
     ref={ref}
-    className='relative flex h-10 cursor-default select-none items-center
-rounded-lg px-2 text-sm outline-none transition-colors duration-75
-active:duration-0 aria-selected:bg-neutral-50
+    className='relative flex h-10 cursor-default select-none flex-row
+items-center gap-1 rounded-lg px-2 text-sm outline-none transition-colors
+duration-75 active:duration-0 aria-selected:bg-neutral-50
 aria-selected:active:bg-neutral-100 data-[disabled]:pointer-events-none
 data-[disabled]:opacity-50 dark:aria-selected:bg-neutral-800
 dark:aria-selected:active:bg-neutral-700'
     {...props}
   >
+    {parentDoc && (
+      <>
+        <span className='text-neutral-600 dark:text-neutral-400'>
+          {parentDoc.title}
+        </span>
+        <span className='text-neutral-600 dark:text-neutral-400'>/</span>
+      </>
+    )}
     {doc.title}
   </CommandItem>
 ));
