d42557cbe056415ab46d0bce783e95e12767c79d
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-11-29 13:51:16 +0200

    Update `max-h-screen` to use dvh instead of vh

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,6 +48,7 @@ const config: Config = {
       maxWidth: { '8xl': '90rem' },
       spacing: { inherit: 'inherit' },
       height: { screen: '100dvh' },
+      maxHeight: { screen: '100dvh' },
       typography: () => ({
         DEFAULT: {
           css: {
