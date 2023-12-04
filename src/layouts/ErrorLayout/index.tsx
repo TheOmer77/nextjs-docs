554dec59edcc2d26c08f80120905d5ac1811372c
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-12-04 22:21:37 +0200

    Update default props & fix CSS in ErrorLayout

--- src/layouts/ErrorLayout/index.tsx
+++ src/layouts/ErrorLayout/index.tsx
@@ -1,23 +1,27 @@
-import type { PropsWithChildren } from 'react';
+import type { PropsWithChildren, ReactNode } from 'react';
 
 export type ErrorLayoutProps = PropsWithChildren<{
-  code: number;
-  text: string;
+  code: ReactNode;
+  text: ReactNode;
 }>;
 
-export const ErrorLayout = ({ code, text, children }: ErrorLayoutProps) => (
+export const ErrorLayout = ({
+  code = 'Error',
+  text = 'An error occurred.',
+  children,
+}: ErrorLayoutProps) => (
   <div
-    className='flex min-h-[calc(100dvh-4rem)] w-full flex-col items-center
+    className='flex min-h-[calc(100dvh-8rem)] w-full flex-col items-center
 justify-center pb-16'
   >
     <h1
-      className='text-9xl font-extrabold leading-none tracking-tight
+      className='m-0 text-9xl font-extrabold leading-none tracking-tight
 text-danger-800 dark:text-danger-200 sm:text-[16rem] md:text-[12rem]
 lg:text-[16rem]'
     >
       {code}
     </h1>
-    <p className='text-center text-xl text-neutral-700 dark:text-neutral-300'>
+    <p className='m-0 text-center text-xl text-neutral-700 dark:text-neutral-300'>
       {text}
     </p>
     {children}
