7275b6d38bc4c4ff4770ca0c1e430428ca3ff6f7
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-08 12:22:00 +0300

    Remove `@svgr/webpack`

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,6 @@
 import { withContentlayer } from 'next-contentlayer2';
 
-/**
- * Enable importing SVGs in React & MDX.
- * @see [SVGR docs](https://react-svgr.com/docs/next/#usage) (modified to include MDX)
- * @type {import('next').NextConfig['webpack']}
- */
-const webpackConfig = config => {
-  // Grab the existing rule that handles SVG imports
-  const fileLoaderRule = config.module.rules.find(rule =>
-    rule.test?.test?.('.svg')
-  );
-
-  config.module.rules.push(
-    // Reapply the existing rule, but only for svg imports not ending in ?react
-    {
-      ...fileLoaderRule,
-      test: /\.svg$/i,
-      resourceQuery: { not: /react/ },
-    },
-    // Convert *.svg imports ending with ?react to React components
-    {
-      test: /\.svg$/i,
-      issuer: /\.([jt]s|md)x?$/,
-      resourceQuery: /react/, // *.svg?react
-      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
-    }
-  );
-
-  // Modify the file loader rule to ignore *.svg, since we have it handled now.
-  fileLoaderRule.exclude = /\.svg$/i;
-
-  return config;
-};
-
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: webpackConfig,
-};
+const nextConfig = { reactStrictMode: true };
 
 const config = withContentlayer(nextConfig);
 export default config;
