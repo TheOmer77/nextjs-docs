6e6d0e33bebc9d0797ea01d113d9d76138a81a35
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-08 15:42:03 +0300

    Tweak search dialog animation duration

--- src/components/layout/search/search-dialog.tsx
+++ src/components/layout/search/search-dialog.tsx
@@ -12,9 +12,9 @@ import { SearchCommand } from './search-command';
 const SearchDialog = ({ open, onOpenChange, children }: DialogProps) => (
   <DialogRoot open={open} onOpenChange={onOpenChange}>
     <DialogPortal>
-      <DialogOverlay className='fixed inset-0 z-50 bg-black/50 data-[state=closed]:animate-fade-out data-[state=open]:animate-fade-in' />
+      <DialogOverlay className='fixed inset-0 z-50 bg-black/50 duration-300 data-[state=closed]:animate-fade-out data-[state=open]:animate-fade-in' />
       <DialogContent
-        className='fixed start-1/2 top-4 z-50 grid w-[calc(100%-2rem)] max-w-md -translate-x-1/2 gap-4 rounded-lg bg-white shadow-lg shadow-neutral-950/20 [--zoom-translate-x:-50%] focus-visible:outline-none data-[state=closed]:animate-zoom-out data-[state=open]:animate-zoom-in sm:top-20 md:top-32 rtl:translate-x-1/2 rtl:[--zoom-translate-x:50%] dark:bg-neutral-900'
+        className='fixed start-1/2 top-4 z-50 grid w-[calc(100%-2rem)] max-w-md -translate-x-1/2 gap-4 rounded-lg bg-white shadow-lg shadow-neutral-950/20 duration-300 [--zoom-translate-x:-50%] focus-visible:outline-none data-[state=closed]:animate-zoom-out data-[state=open]:animate-zoom-in sm:top-20 md:top-32 rtl:translate-x-1/2 rtl:[--zoom-translate-x:50%] dark:bg-neutral-900'
         aria-describedby={undefined}
       >
         <DialogTitle className='sr-only'>Search docs...</DialogTitle>
