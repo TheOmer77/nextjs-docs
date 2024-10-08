39d1a30220568de521a69d6dfbbbbd4fcf0eff9c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-08 14:47:54 +0300

    Remove unused SVG import types

--- src/types/svg.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg?react' {
-  import type { FunctionComponent, ComponentProps } from 'react';
-
-  const ReactComponent: FunctionComponent<
-    ComponentProps<'svg'> & { title?: string }
-  >;
-
-  export default ReactComponent;
-}
