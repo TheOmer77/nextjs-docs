554490273557785f3e63f38ab869de9336d9a595
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-10 20:04:40 +0300

    Remove double buttons in NavLink

--- src/components/layout/nav/nav-link.tsx
+++ src/components/layout/nav/nav-link.tsx
@@ -12,28 +12,16 @@ export const NavLink = ({
 }: ComponentPropsWithoutRef<'a'>) => {
   const LinkComp = href?.startsWith?.('/') ? Link : 'a';
   return (
-    <>
-      <Button
-        asChild
-        variant='flat'
-        size='lg'
-        icon
-        className={cn('md:hidden', className)}
-      >
-        <LinkComp {...props} href={href as string}>
-          {children}
-        </LinkComp>
-      </Button>
-      <Button
-        asChild
-        variant='flat'
-        icon
-        className={cn('hidden md:inline-flex', className)}
-      >
-        <LinkComp {...props} href={href as string}>
-          {children}
-        </LinkComp>
-      </Button>
-    </>
+    <Button
+      asChild
+      variant='flat'
+      size='lg'
+      icon
+      className={cn('md:size-10', className)}
+    >
+      <LinkComp {...props} href={href as string}>
+        {children}
+      </LinkComp>
+    </Button>
   );
 };
