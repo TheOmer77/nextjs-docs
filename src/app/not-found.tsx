302f50bbe71a9df779f0a94ef2e50e620e4fa5cb
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-25 20:07:08 +0300

    Update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,62 @@
-import { ErrorLayout } from '@/components/layout/error';
-import { Mdx } from '@/components/layout/mdx';
-import { allDocs, config, notFoundPageName } from '@/constants/contentlayer';
+import { notFound } from 'next/navigation';
+import Link from 'fumadocs-core/link';
 
-export const generateMetadata = () => {
-  const doc = allDocs.find(doc => doc._raw.flattenedPath === notFoundPageName);
-  return {
-    title: doc?.title
-      ? config.titleTemplate.replace('%s', doc.title)
-      : config.title,
-  };
+import { ErrorLayout } from '@/components/layout/error';
+import { HomeLayout } from '@/components/layout/home';
+import { Alert, MdxBlockquote } from '@/components/layout/mdx';
+import { cn } from '@/lib/cn';
+import { source } from '@/lib/source';
+
+import type { MDXComponents } from 'mdx/types';
+
+const mdxComponents = {
+  a: Link,
+  blockquote: MdxBlockquote,
+  Alert,
+
+  HomeLayout,
+  ErrorLayout,
+} satisfies MDXComponents;
+
+export const generateStaticParams = async () => source.generateParams();
+
+export const generateMetadata = async (props: {
+  params: Promise<{ slug?: string[] }>;
+}) => {
+  const params = await props.params;
+  const page = source.getPage(params.slug);
+  if (!page) notFound();
+
+  const { title, description } = page.data;
+  return { title, description };
 };
 
-const NotFoundPage = () => {
-  const doc = allDocs.find(doc => doc._raw.flattenedPath === notFoundPageName);
+const NotFoundPage = async () => {
+  const page = source.getPage(['_not-found']);
+  if (!page)
+    return (
+      <div className='py-8'>
+        <ErrorLayout code={404} text='Not found' />;
+      </div>
+    );
 
-  return doc ? (
-    <Mdx doc={doc} />
-  ) : (
-    <div className='py-8'>
-      <ErrorLayout code={404} text='Not found' />;
+  const { body: MDX, title, showSidebar, showTitle } = page.data;
+
+  return (
+    <div
+      className={cn(
+        'prose mx-auto w-[calc(100vw-2rem)] max-w-8xl py-8 dark:prose-invert',
+        showSidebar && 'md:max-w-[calc(100vw-22rem)] lg:max-w-3xl'
+      )}
+    >
+      {title && showTitle && (
+        <h1 className='text-5xl font-extrabold tracking-tight sm:text-[3.5rem]'>
+          {title}
+        </h1>
+      )}
+      <main>
+        <MDX components={mdxComponents} />
+      </main>
     </div>
   );
 };
