22e4987abfe007367882529462d0f1614c666132
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-10-25 19:53:45 +0300

    Add custom document schema in source config

--- source.config.ts
+++ source.config.ts
@@ -1,5 +1,19 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import {
+  defineDocs,
+  defineConfig,
+  frontmatterSchema,
+} from 'fumadocs-mdx/config';
+import { z } from 'zod';
 
-export const { docs, meta } = defineDocs({ dir: 'data' });
+export const { docs, meta } = defineDocs({
+  dir: 'data',
+  docs: {
+    schema: frontmatterSchema.extend({
+      title: z.string().default(''),
+      showTitle: z.boolean().default(true),
+      showSidebar: z.boolean().default(true),
+    }),
+  },
+});
 
 export default defineConfig();
