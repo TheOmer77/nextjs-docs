3d0c0f9dd42296690e39779432344193008314b0
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-11 16:29:34 +0200

    Update nav to only show logo in print

--- src/components/layout/nav/nav.tsx
+++ src/components/layout/nav/nav.tsx
@@ -16,7 +16,7 @@ const NavDrawerButton = () => {
       variant='flat'
       size='lg'
       icon
-      className='ms-4 md:hidden'
+      className='ms-4 md:hidden print:hidden'
       aria-label='Open sidebar'
       onClick={() => openModal('nav-drawer')}
     >
@@ -26,16 +26,16 @@ const NavDrawerButton = () => {
 };
 
 export const Nav = () => (
-  <nav className='fixed start-1/2 top-0 z-20 mx-auto flex h-16 w-full max-w-8xl -translate-x-1/2 flex-row items-center justify-between bg-background md:bg-transparent rtl:translate-x-1/2'>
+  <nav className='fixed start-1/2 top-0 z-20 mx-auto flex h-16 w-full max-w-8xl -translate-x-1/2 flex-row items-center justify-between bg-background md:bg-transparent rtl:translate-x-1/2 print:static print:block print:h-auto print:translate-x-0'>
     <Suspense>
       <NavDrawerButton />
     </Suspense>
     <Logo
       /* Setting `display: none` breaks SVG gradients in the logo, so using 
       a different method for hiding on mobile */
-      className='invisible absolute size-0 md:visible md:relative md:h-16'
+      className='invisible absolute md:visible md:relative md:h-16 print:visible print:relative print:h-auto print:w-full print:p-0'
     />
-    <div className='flex h-full grow flex-row items-center justify-end gap-2 bg-background px-4'>
+    <div className='flex h-full grow flex-row items-center justify-end gap-2 bg-background px-4 print:hidden'>
       <Suspense fallback={<SearchButton disabled />}>
         <Search />
       </Suspense>
