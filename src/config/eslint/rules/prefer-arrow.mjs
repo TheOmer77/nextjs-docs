bc0d5508f61f5bb03ce431b913066ef6969d1f5d
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-12 12:49:16 +0200

    Fix broken preferArrowFunctions rule
    Some update of this plugin broke the default export, so now `.default`
    must be appended to the plugin

--- src/config/eslint/rules/prefer-arrow.mjs
+++ src/config/eslint/rules/prefer-arrow.mjs
@@ -3,7 +3,8 @@ import preferArrowFunctions from 'eslint-plugin-prefer-arrow-functions';
 /** @type {import("typescript-eslint").ConfigWithExtends} */
 export const preferArrow = {
   name: 'Prefer arrow functions',
-  plugins: { 'prefer-arrow-functions': preferArrowFunctions },
+  // eslint-plugin-prefer-arrow-functions export is weird (.default)
+  plugins: { 'prefer-arrow-functions': preferArrowFunctions.default },
   rules: {
     'prefer-arrow-callback': 'warn',
     'prefer-arrow-functions/prefer-arrow-functions': [
