151393a404a177049195b5e70875226b08a759e4
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-12 16:56:37 +0200

    Add tooltip to mobile search button

--- src/components/layout/search/search-button.tsx
+++ src/components/layout/search/search-button.tsx
@@ -2,6 +2,7 @@ import type { ComponentPropsWithoutRef } from 'react';
 import { SearchIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { Tooltip } from '@/components/ui/tooltip';
 
 import { SearchBoxButton } from './search-box-button';
 
@@ -10,17 +11,19 @@ export const SearchButton = ({
   disabled,
 }: Pick<ComponentPropsWithoutRef<'button'>, 'onClick' | 'disabled'>) => (
   <>
-    <Button
-      variant='flat'
-      size='lg'
-      icon
-      className='sm:hidden'
-      aria-label='Search docs'
-      onClick={onClick}
-      disabled={disabled}
-    >
-      <SearchIcon />
-    </Button>
+    <Tooltip content='Search'>
+      <Button
+        variant='flat'
+        size='lg'
+        icon
+        className='sm:hidden'
+        aria-label='Search docs'
+        onClick={onClick}
+        disabled={disabled}
+      >
+        <SearchIcon />
+      </Button>
+    </Tooltip>
     <SearchBoxButton
       onClick={onClick}
       disabled={disabled}
