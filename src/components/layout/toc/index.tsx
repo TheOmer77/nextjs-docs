4697d409c3e0ac1e7662f9f0d5224cc238ed6a78
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-14 17:01:30 +0200

    Add TOC component

---
+++ src/components/layout/toc/index.tsx
@@ -0,0 +1,6 @@
+export const Toc = () => (
+  <div className='sticky top-0 p-4 pt-8 text-sm print:hidden'>
+    <h3 className='mb-2 font-bold'>On this page</h3>
+    <span className='text-muted-foreground'>Table of contents TBD</span>
+  </div>
+);
