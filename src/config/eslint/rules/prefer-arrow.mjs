936dd166cf5e179d5c6a26053b0300eb9d407267
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-14 12:24:12 +0200

    Remove fix for `eslint-plugin-prefer-arrow-functions`

--- src/config/eslint/rules/prefer-arrow.mjs
+++ src/config/eslint/rules/prefer-arrow.mjs
@@ -3,8 +3,7 @@ import preferArrowFunctions from 'eslint-plugin-prefer-arrow-functions';
 /** @type {import("typescript-eslint").ConfigWithExtends} */
 export const preferArrow = {
   name: 'Prefer arrow functions',
-  // eslint-plugin-prefer-arrow-functions export is weird (.default)
-  plugins: { 'prefer-arrow-functions': preferArrowFunctions.default },
+  plugins: { 'prefer-arrow-functions': preferArrowFunctions },
   rules: {
     'prefer-arrow-callback': 'warn',
     'prefer-arrow-functions/prefer-arrow-functions': [
