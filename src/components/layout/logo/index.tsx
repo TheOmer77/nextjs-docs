9ba0bca7c28cb00965f9686a80668908d69b7302
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-16 14:44:16 +0200

    Don't use special logo doc for logo

--- src/components/layout/logo/index.tsx
+++ src/components/layout/logo/index.tsx
@@ -1,32 +1,23 @@
 import type { ComponentPropsWithoutRef } from 'react';
-import { MDXContent } from '@content-collections/mdx/react';
 
 import { cn } from '@/lib/cn';
-import { allDocs, config, navLogoName } from '@/constants/docs';
+import { config } from '@/constants/docs';
 
 import { SidebarLink } from '../sidebar/sidebar-link';
 
 export const Logo = ({
   className,
   ...props
-}: ComponentPropsWithoutRef<'header'>) => {
-  // TODO: Remove this
-  const logoDoc = allDocs.find(doc => doc._meta.path === navLogoName);
-
-  return (
-    <header
-      {...props}
-      className={cn(`flex h-16 flex-row items-center px-4 md:w-80`, className)}
+}: ComponentPropsWithoutRef<'header'>) => (
+  <header
+    {...props}
+    className={cn(`flex h-16 flex-row items-center px-4 md:w-80`, className)}
+  >
+    <SidebarLink
+      href='/'
+      className='text-2xl font-bold tracking-tight text-primary'
     >
-      <SidebarLink href='/'>
-        {logoDoc ? (
-          <MDXContent code={logoDoc.body.code} />
-        ) : (
-          <span className='text-2xl font-bold tracking-tight text-primary'>
-            {config.title}
-          </span>
-        )}
-      </SidebarLink>
-    </header>
-  );
-};
+      {config.title}
+    </SidebarLink>
+  </header>
+);
