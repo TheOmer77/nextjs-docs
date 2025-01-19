b4b13f5e937f9c8b40b6fc835dc23b6b491000ec
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-19 14:24:52 +0200

    Use links from config in NavLinks

--- src/components/layout/nav/links.tsx
+++ src/components/layout/nav/links.tsx
@@ -1,43 +1,69 @@
 import type { ComponentPropsWithoutRef } from 'react';
 import Link from 'next/link';
-import { MDXContent } from '@content-collections/mdx/react';
+import { LinkIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { Tooltip } from '@/components/ui/tooltip';
 import { cn } from '@/lib/cn';
-import { allDocs, navLinksName } from '@/constants/docs';
+import { config } from '@/constants/docs';
 
-import { mdxComponents } from '../mdx';
+type ConfigLink = (typeof config.links)[number];
+type NavLinkProps = ConfigLink &
+  Omit<ComponentPropsWithoutRef<'a'>, keyof ConfigLink | 'children'>;
+
+const GithubIcon = () => (
+  <svg
+    width='1em'
+    height='1em'
+    viewBox='0 0 98 96'
+    xmlns='http://www.w3.org/2000/svg'
+    fill='currentColor'
+  >
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z'
+    />
+  </svg>
+);
+
+const LINK_ICONS = {
+  DEFAULT: <LinkIcon />,
+  github: <GithubIcon />,
+};
 
 export const NavLink = ({
   href,
+  label,
+  type,
   className,
-  children,
   ...props
-}: ComponentPropsWithoutRef<'a'>) => {
-  const LinkComp = href?.startsWith?.('/') ? Link : 'a';
+}: NavLinkProps) => {
+  const icon =
+    LINK_ICONS[
+      type in LINK_ICONS ? (type as keyof typeof LINK_ICONS) : 'DEFAULT'
+    ];
   return (
-    <Button
-      asChild
-      variant='flat'
-      size='lg'
-      icon
-      className={cn('md:size-10', className)}
-    >
-      <LinkComp {...props} href={href as string}>
-        {children}
-      </LinkComp>
-    </Button>
+    <Tooltip content={label}>
+      <Button
+        asChild
+        variant='flat'
+        size='lg'
+        icon
+        className={cn('md:size-10', className)}
+      >
+        <Link {...props} href={href as string}>
+          {icon}
+        </Link>
+      </Button>
+    </Tooltip>
   );
 };
 
-export const NavLinks = () => {
-  // TODO: Remove this, move links to config.json
-  const linksDoc = allDocs.find(doc => doc._meta.path === navLinksName);
-  if (linksDoc)
-    return (
-      <MDXContent
-        code={linksDoc.body.code}
-        components={{ ...mdxComponents, NavLink }}
-      />
-    );
-};
+export const NavLinks = () => (
+  <>
+    {config.links.map(({ href, label, type }) => (
+      <NavLink key={label} href={href} label={label} type={type} />
+    ))}
+  </>
+);
