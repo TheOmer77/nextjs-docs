8f670d4386b077ad010bd90c828c7d77a91332ce
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-20 16:45:48 +0200

    Tweak NotFoundPage structure

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,15 +5,13 @@ export const generateMetadata = () => ({
 });
 
 const NotFoundPage = () => (
-  <main className='py-8'>
-    <div className='flex min-h-[calc(100dvh-8rem)] w-full flex-col items-center justify-center pb-16'>
-      <h1 className='m-0 text-9xl font-extrabold leading-none tracking-tight text-danger-800 sm:text-[16rem] md:text-[12rem] lg:text-[16rem] dark:text-danger-200'>
-        404
-      </h1>
-      <p className='m-0 text-center text-xl text-muted-foreground'>
-        I&apos;m not sure what you were looking for, but it&apos;s not here.
-      </p>
-    </div>
+  <main className='flex min-h-[calc(100dvh-8rem)] w-full flex-col items-center justify-center'>
+    <h1 className='m-0 text-9xl font-extrabold leading-none tracking-tight text-danger-800 sm:text-[16rem] md:text-[12rem] lg:text-[16rem] dark:text-danger-200'>
+      404
+    </h1>
+    <p className='m-0 text-center text-xl text-muted-foreground'>
+      I&apos;m not sure what you were looking for, but it&apos;s not here.
+    </p>
   </main>
 );
 
