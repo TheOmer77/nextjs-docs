658c88a9501b270abeed2fb1f00410686e19105a
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-21 14:07:38 +0200

    Update 404 page design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { ArrowLeftIcon } from 'lucide-react';
+
 import { config } from '@/constants/docs';
 
 export const generateMetadata = () => ({
@@ -5,13 +8,20 @@ export const generateMetadata = () => ({
 });
 
 const NotFoundPage = () => (
-  <main className='flex min-h-[calc(100dvh-8rem)] w-full flex-col items-center justify-center'>
-    <h1 className='m-0 text-9xl font-extrabold leading-none tracking-tight text-danger-800 sm:text-[16rem] md:text-[12rem] lg:text-[16rem] dark:text-danger-200'>
+  <main className='flex min-h-[calc(100dvh-theme(spacing.32))] w-full flex-col items-center justify-center px-4 text-center'>
+    <h1 className='font-mono text-[min(calc(50dvw-1rem),25rem)] font-extrabold leading-none tracking-tighter text-muted-foreground/30'>
       404
     </h1>
-    <p className='m-0 text-center text-xl text-muted-foreground'>
-      I&apos;m not sure what you were looking for, but it&apos;s not here.
+    <p className='mb-8 leading-tight text-muted-foreground'>
+      I&apos;m not sure what you were looking for... but it&apos;s not here.
     </p>
+    <Link
+      href='/'
+      className='group flex flex-row items-center text-sm font-medium hover:underline'
+    >
+      <ArrowLeftIcon className='me-2 transition-transform group-hover:-translate-x-1' />
+      Return home
+    </Link>
   </main>
 );
 
