195238354a44468b881116f2f7ec95fbe2f92158
Omer <27678413+TheOmer77@users.noreply.github.com>
2025-01-27 20:29:11 +0200

    Render title in CodeBlock component

--- src/components/layout/mdx/code-block.tsx
+++ src/components/layout/mdx/code-block.tsx
@@ -1,6 +1,21 @@
 import type { ComponentPropsWithoutRef } from 'react';
 
+import { cn } from '@/lib/cn';
+
 export const CodeBlock = ({
   children,
+  title,
+  className,
   ...props
-}: ComponentPropsWithoutRef<'pre'>) => <pre {...props}>{children}</pre>;
+}: ComponentPropsWithoutRef<'pre'>) => (
+  <>
+    {title && (
+      <div className='rounded-t-lg border-b bg-[--tw-prose-pre-bg] p-2 font-mono text-sm text-muted-foreground '>
+        {title}
+      </div>
+    )}
+    <pre {...props} className={cn(title && 'mt-0 rounded-t-none', className)}>
+      {children}
+    </pre>
+  </>
+);
